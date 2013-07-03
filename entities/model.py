47601fe5bcc0373a0828fd05c74f1eb2d5c0878c
eladio <sr.eladio@gmail.com>
2013-07-03 19:28:15 +0200

    new model 'WikiPageVersionRecord'

--- entities/model.py
+++ entities/model.py
@@ -42,6 +42,12 @@ class User(db.Model):
 		return query.get()
 
 	@classmethod
+	def get_by_pswd(cls, pswd):
+		query = db.Query(User)
+		query.filter('password =', pswd)
+		returnquery.get()
+
+	@classmethod
 	def get_by_id(cls, user_id):
 		key = db.Key.from_path('User', int(user_id))
 		return db.get(key)
@@ -54,8 +60,11 @@ class WikiPage(db.Model):
 	title = db.StringProperty()
 	content = db.TextProperty()
 	created = db.DateProperty(auto_now_add=True)
-	edit = db.DateProperty(auto_now_add=True)
+	version = db.IntegerProperty()
 
+  	def get_version_record(self):
+  		return self.version_record
+  		
 	@classmethod
 	def get_by_url(cls, url):
 		query = db.Query(WikiPage)
@@ -64,8 +73,28 @@ class WikiPage(db.Model):
 
 	@classmethod
 	def create(cls, url, title, content):
-		return WikiPage(url=url, title=title, content=content)
+		return WikiPage(url=url, title=title, content=content, version=1)
 
 	@classmethod
   	def get_all(cls):
   		return db.GqlQuery("SELECT * FROM WikiPage ORDER BY created DESC")
+
+#
+# Entity Wiki Page Version Record
+#
+class WikiPageVersionRecord(db.Model):
+	wiki_page = db.ReferenceProperty(WikiPage, collection_name='version_record')
+	url = db.LinkProperty()
+	title = db.StringProperty()
+	content = db.TextProperty()
+	created = db.DateProperty(auto_now_add=True)
+	version = db.IntegerProperty()
+ 
+ 	@classmethod
+ 	def create(cls, wiki_page):
+ 		url = wiki_page.url
+ 		title = wiki_page.title
+ 		content = wiki_page.content[-100:]
+		created = wiki_page.created
+		version = wiki_page.version
+		return WikiPageVersionRecord(wiki_page=wiki_page, url=url, title=title, content=content, created=created, version=version)
