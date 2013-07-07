856c0c7a3041292e14c8587487f4ae4a65173b5c
eladio <sr.eladio@gmail.com>
2013-07-07 20:23:37 +0200

    handlers refactored

--- handlers/wiki.py
+++ handlers/wiki.py
@@ -16,13 +16,9 @@ from lib import markdown
 #
 class WikiPage(main.MainHandler):
 	def get(self, page):
-
 		# search for the wiki page in the DB
 		version = self.request.get('v')
-		if version:
-			wiki_page = self.get_old_version(version)
-		else:
-			wiki_page = self.get_latest_version()
+		wiki_page = self.get_wiki_page(page, version)
 
 		# if doesn't exist, redirect to the edit page
 		if wiki_page is None:
@@ -31,43 +27,43 @@ class WikiPage(main.MainHandler):
 		# if it already exist, render the wiki page
 		else:
 			content = markdown.markdown(wiki_page.content)
-			self.render('/wiki/wiki-page.html', page=page, version=version, title=wiki_page.title, content=content)
+			params = {'page':page, 
+					  'version':version, 
+					  'title':wiki_page.title, 
+					  'content':content}
+			self.render('/wiki/wiki-page.html', **params)
 
-	def get_old_version(self, version):
-		url = self.request.url.split('?')[0]
+	def get_wiki_page(self, page, version):
+		url = self.buid_wiki_page_url(page)
+		if version:
+			return self.get_old_version(url, version)
+		else:
+			return self.get_latest_version(url)
+
+	def get_old_version(self, url, version):
 		version_record = self.get_wiki_page_version_record(url)
 		version_record.filter('version = ', int(version))
 		return version_record.get()
 
-	def get_latest_version(self):
-		url = self.request.url
-		return self.get_wiki_page(url)
-
-	def get_wiki_page(self, url):
+	def get_latest_version(self, url):
 		return model.WikiPage.get_by_url(url)
 
 	def get_wiki_page_version_record(self, url):
-		wiki_page = self.get_wiki_page(url)
+		wiki_page = self.get_latest_version(url)
 		version_record = wiki_page.version_record
 		return version_record.order('version')
+
+	def buid_wiki_page_url(self, page):
+		return self.uri_for('wiki-page', _full=True, page=page)
 
 #
 # Edit Page Handler
 #
 class EditPage(WikiPage):
 	@decorator.login_required
-	def get(self, path, error=""):
-
-		page_url = self.uri_for('wiki-page', _full=True, page=path[1:])
-		wiki_page = self.get_wiki_page(page_url)
-
+	def get(self, page, error=""):		
 		version = self.request.get('v')
-		if version:
-			version_record = self.get_wiki_page_version_record(page_url)
-			version_record.filter('version = ', int(version))
-			wiki_page = version_record.get()
-		else:
-			wiki_page = model.WikiPage.get_by_url(page_url)
+		wiki_page = self.get_wiki_page(page, version)
 		
 		# if the wiki page exist, get the content and the title
 		if wiki_page is not None:
@@ -75,32 +71,33 @@ class EditPage(WikiPage):
 			title = wiki_page.title
 		else:
 			content = ''
-			title = path[1:]
+			title = page
 
 		# render the edit page
-		self.render('/wiki/edit-page.html', page=path, title=title, content=content, error=error)		
+		params = {'page':page, 'title':title, 'content':content, 'error':error}
+		self.render('/wiki/edit-page.html', **params)		
 
-	def post(self, path):
+	def post(self, page):
 		title = self.request.get('wiki-title')
 		content = self.request.get('wiki-content')
-		page_url = self.uri_for('wiki-page', _full=True, page=path[1:])
 
+		# check for empty title or content
 		if title == '' or content == '':
 			error = 'Title and content, please!'
-			self.get(path, error)
+			self.get(page, error)
 			return
 
 		# before doing anything, sanitize the input
 		# deleting all html tags
 		content = validation.delete_html_tags(content)
-
-		# search for the wiki page in the DB
-		wiki_page = model.WikiPage.get_by_url(page_url)
+		
+		# get the wiki page from the DB
+		wiki_page = self.get_wiki_page(page, None)
 
 		# if doesn't exist, create new entity
 		if wiki_page is None:
 			wiki_page = model.WikiPage.create(page_url, title, content)
-
+		
 		# if it already exist, setting the new title and content
 		else:
 			wiki_page.title = title
@@ -112,9 +109,23 @@ class EditPage(WikiPage):
 
 		# update de versions record
 		model.WikiPageVersionRecord.create(wiki_page=wiki_page).put()
-
+		
 		# redirect to the wiki page
-		self.redirect_to('wiki-page', page=path[1:])
+		self.redirect_to('wiki-page', page=page)
+
+#
+# History Page Handler
+#
+class HistoryPage(WikiPage):
+	def get(self, page):
+		# build the url
+		page_url = self.buid_wiki_page_url(page)
+
+		# get the version record of the wiki page
+		version_record = self.get_wiki_page_version_record(page_url)
+
+		# render
+		self.render('/wiki/history_page.html', version_record=version_record)
 
 #
 # Front Wiki Handler
@@ -129,17 +140,3 @@ class FrontPage(main.MainHandler):
 		for i in range(len(wiki)):
 			wiki[i].content = markdown.markdown(wiki[i].content)
 		return wiki
-
-#
-# History Page Handler
-#
-class HistoryPage(WikiPage):
-	def get(self, page):
-		# build the url
-		page_url = self.uri_for('wiki-page', _full=True, page=page[1:])
-
-		# get the version record of the wiki page
-		version_record = self.get_wiki_page_version_record(page_url)
-
-		# render
-		self.render('/wiki/history_page.html', version_record=version_record)
